b839048709f5760278b81d6a61490a26906acb02
lordgr8t <lordgr8t@gmail.com>
2021-04-08 00:22:18 +0300

    85% adaptive

--- js/main.js
+++ js/main.js
@@ -11,11 +11,45 @@ function closemenu(){
 }
 
 // slider
+
 $('.slider-form__slider').slick({
 	infinite: true,
 	slidesToShow: 2,
-	slidesToScroll: 2
+	slidesToScroll: 2,
+	responsive: [
+	{
+		breakpoint: 1100,
+		settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+		}
+	},
+	{
+		breakpoint: 600,
+		settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		}
+	},
+	{
+		breakpoint: 480,
+		settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		}
+	}
+	]
 });
+
+
+
 
 $("body > section.slider-form > div > button.slick-prev.slick-arrow").text(" ").append('<img src="./img/arrow_back.svg"; />');
 $("body > section.slider-form > div > button.slick-next.slick-arrow").text(" ").append('<img src="./img/arrow_next.svg"; />'); 
+
+// dell
+// $("body > section.slider-form > div > button.slick-prev.slick-arrow").hide();
+// $("body > section.slider-form > div > button.slick-next.slick-arrow").hide();
+// dell
+
